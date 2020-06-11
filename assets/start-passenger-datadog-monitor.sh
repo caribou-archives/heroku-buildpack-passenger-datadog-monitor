26c7007af8add0e4b083304ba74d41c08c964abf
Ben McNiel <ben@adazza.com>
2020-06-10 22:17:56 -0400

    fix(bashsyntax): fix syntax.

--- assets/start-passenger-datadog-monitor.sh
+++ assets/start-passenger-datadog-monitor.sh
@@ -4,15 +4,10 @@ prog="passenger-datadog-monitor"
 PIDFILE=/app/tmp/$prog.pid
 
 
-start() {
-        printf "%s\n" "Starting $prog: "
-        PID=`nohup /app/bin/passenger-datadog-monitor > /app/tmp/passenger-datadog-monitor.log 2>&1 & echo $!`
-        RETVAL=$?
-        if [ $RETVAL -eq 0 ]; then
-          echo "$PID" > $PIDFILE
-          printf "%s\n" "Ok"
-        fi
-        return $RETVAL
-}
-
-return start()
+printf "%s\n" "Starting $prog: "
+PID=`nohup /app/bin/passenger-datadog-monitor > /app/tmp/passenger-datadog-monitor.log 2>&1 & echo $!`
+RETVAL=$?
+if [ $RETVAL -eq 0 ]; then
+  echo "$PID" > $PIDFILE
+  printf "%s\n" "Ok"
+fi
