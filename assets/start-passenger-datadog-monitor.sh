e3ed2a1beb734dc60d8a5529617e0ef016635a0a
Ben McNiel <ben@adazza.com>
2020-06-10 20:35:01 -0400

    feat(Readonly): Slugs are readonly file systems.

--- assets/start-passenger-datadog-monitor.sh
+++ assets/start-passenger-datadog-monitor.sh
@@ -1,2 +1,18 @@
 #!/bin/sh
-service passenger-datadog-monitor start
+RETVAL=0
+prog="passenger-datadog-monitor"
+PIDFILE=/app/tmp/$prog.pid
+
+
+start() {
+        printf "%s\n" "Starting $prog: "
+        PID=`nohup /app/bin/passenger-datadog-monitor > /app/tmp/passenger-datadog-monitor.log 2>&1 & echo $!`
+        RETVAL=$?
+        if [ $RETVAL -eq 0 ]; then
+          echo "$PID" > $PIDFILE
+          printf "%s\n" "Ok"
+        fi
+        return $RETVAL
+}
+
+return start()
